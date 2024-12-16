ae23d2d881e9a8268a740e724948ea6a8d08e42d
sun_nick <nikitasergsol@yandex.ru>
2024-12-16 23:38:26 +0300

    Lab3

---
+++ src/main/java/tech/reliab/course/solntsevns/SolntsevApplication.java
@@ -0,0 +1,11 @@
+package tech.reliab.course.solntsevns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolntsevApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SolntsevApplication.class, args);
+    }
+}
